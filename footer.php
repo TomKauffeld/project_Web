5da23fbf1c3a3f2ef674605d106a2db1601c3fd9
latieulej <latieulej@gmail.com>
2018-04-21 23:23:58 -0400

    Mise en place design

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="ressources/scripts/utils.js"></script>
+    <script type="text/javascript" src="ressources/scripts/blog_api.js"></script>
+    <script type="text/javascript" src="ressources/scripts/app.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</footer>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Near from keyboard</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="ressources/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="ressources/css/login.css">
+    <link rel="stylesheet" href="ressources/css/style.css">
+
+
+</head>
+<body>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Near from keyboard</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Actualités</a></li>
+                    <li><a href="#">Tests</a></li>
+                    <li><a href="#">Vidéo</a></li>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Connexion</a></li>
+                    <li><a href="#">Inscription</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
