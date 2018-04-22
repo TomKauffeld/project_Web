409e7f118ef5644cd185a64b684f2646b7f5a3a3
latieulej <latieulej@gmail.com>
2018-04-21 23:45:00 -0400

    Design menu

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="ressources/css/login.css">
     <link rel="stylesheet" href="ressources/css/style.css">
 
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 </head>
 <body>
@@ -33,11 +34,50 @@
                     <li><a href="#">Vidéo</a></li>
                 </ul>
 
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Connexion</a></li>
-                    <li><a href="#">Inscription</a></li>
+                <ul id="not_logged_in" class="nav navbar-nav navbar-right w3-row">
+                    <script type="text/javascript">$("#not_logged_in").hide()</script>
+                    <li><button type="button" class="w3-button" onclick="document.getElementById('login_page').style.display='block'">Se connecter</button></li>
+                    <li><button type="button" class="w3-button" onclick="document.getElementById('create_page').style.display='block'">Cr&eacute;er un compte</button></li>
+
+                    <div id="login_page" class="w3-modal w3-display-middle">
+                        <div id="form_login" class="w3-col s12 m7 l3 w3-display-container w3-display-middle">
+                            <span onclick="document.getElementById('login_page').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+                            <form>
+                                <label for="login_username"><b>Nom d'utilisateur</b></label>
+                                <input autocomplete="username" id="login_username" type="text" placeholder="Entrez le nom d'utilisateur" required>
+                                
+                                <label for="login_password"><b>Mot de passe</b></label>
+                                <input autocomplete="password" id="login_password" type="password" placeholder="Entrez le mot de passe" required>
+                            </form>
+                            <button id="login">Se connecter</button>
+                        </div>
+                    </div>
+
+                    <div id="create_page" class="w3-modal w3-display-middle">
+                        <div id="form_create" class="w3-col s12 m7 l3 w3-display-container w3-display-middle">
+                            <span onclick="document.getElementById('create_page').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+                            <form>
+                                <label for="create_username"><b>Nom d'utilisateur</b></label>
+                                <input autocomplete="username" id="create_username" type="text" placeholder="Entrez le nom d'utilisateur" required>
+                                
+                                <label for="create_password"><b>Mot de passe</b></label>
+                                <input autocomplete="password" id="create_password" type="password" placeholder="Entrez le mot de passe" required>
+                                
+                                <label for="create_repeat"><b>Repetez le mot de passe</b></label>
+                                <input autocomplete="password" id="create_repeat" type="password" placeholder="Repetez le mot de passe" required>
+                            </form>
+                            <button id="create">Cr&eacute;er une nouvelle compte</button>
+                        </div>
+                    </div>
+                </ul>
+
+                <ul id="logged_in" class="nav navbar-nav navbar-right">
+                    <script type="text/javascript">$("#logged_in").hide()</script>
+
+                    <li><button id="logout" class="w3-button">Se d&eacute;connecter</button></li>
+                    <li><span id="welcomeMsg">Bienvenue utilisateur</span></li>
+
                 </ul>
             </div>
         </div>
     </nav>
-</body>
