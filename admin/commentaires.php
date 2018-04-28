fc292d5b3a26b625694821117352e2006a230120
latieulej <latieulej@gmail.com>
2018-04-28 17:49:20 -0400

    Page gestion des commentaires

---
+++ admin/commentaires.php
@@ -0,0 +1,38 @@
+<?php include('header.php'); ?>
+
+<h2>Gestion des utilisateurs</h2>
+
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr>
+            <th>Contenu</th>
+            <th>Article</th>
+            <th>Auteur</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="comment-table">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut nisl vitae neque egestas aliquet. Vivamus nunc quam, sollicitudin nec metus sed, malesuada finibus libero. Donec molestie lorem a scelerisque sollicitudin. Curabitur sed scelerisque ex. Nam sollicitudin luctus convallis. Nunc ullamcorper sagittis rhoncus.</td>
+            <td>Lorem ipsum dolor sit amet</td>
+            <td>Julie Latieule</td>
+            <td><a href="#" alt="Supprimer l'article" class="icon-delete"><i class="fa fa-times"></i></a></td>
+        </tr>
+        <tr>
+            <td class="comment-table">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut nisl vitae neque egestas aliquet. Vivamus nunc quam, sollicitudin nec metus sed, malesuada finibus libero. Donec molestie lorem a scelerisque sollicitudin. Curabitur sed scelerisque ex. Nam sollicitudin luctus convallis. Nunc ullamcorper sagittis rhoncus.</td>
+            <td>Lorem ipsum dolor sit amet</td>
+            <td>Julie Latieule</td>
+            <td><a href="#" alt="Supprimer l'article" class="icon-delete"><i class="fa fa-times"></i></a></td>
+        </tr>
+        <tr>
+            <td class="comment-table">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut nisl vitae neque egestas aliquet. Vivamus nunc quam, sollicitudin nec metus sed, malesuada finibus libero. Donec molestie lorem a scelerisque sollicitudin. Curabitur sed scelerisque ex. Nam sollicitudin luctus convallis. Nunc ullamcorper sagittis rhoncus.</td>
+            <td>Lorem ipsum dolor sit amet</td>
+            <td>Julie Latieule</td>
+            <td><a href="#" alt="Supprimer l'article" class="icon-delete"><i class="fa fa-times"></i></a></td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+<?php include('footer.php'); ?>
